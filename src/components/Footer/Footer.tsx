0d4c6d06938bbc301b3c4be628322cfecb901d3e
Ezaz Ahmed <ezaznahmed@gmail.com>
2023-08-21 22:58:50 +0600

    ➖ remove all previous files

--- src/components/Footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <div
-      className={`fixed z-40 top-0 inset-0 bg-black/30 dark:bg-black/50 transition-opacity will-change-[opacity]`}
-    />
-  )
-}
-
-export default Footer
--- src/components/Header/Header.tsx
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-  return (
-    <div
-      className={`fixed z-40 top-0 inset-0 bg-black/30 dark:bg-black/50 transition-opacity will-change-[opacity]`}
-    />
-  )
-}
-
-export default Header
