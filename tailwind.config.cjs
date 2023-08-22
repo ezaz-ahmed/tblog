90e26894d4f407a3857e1c41f38daf1564677d9b
Ezaz Ahmed <ezaznahmed@gmail.com>
2023-08-22 10:05:31 +0600

    ✨ working on navbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,64 @@
+function withOpacity(variableName) {
+	return ({ opacityValue }) => {
+		if (opacityValue !== undefined) {
+			return `rgba(var(${variableName}), ${opacityValue})`;
+		}
+		return `rgb(var(${variableName}))`;
+	};
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	darkMode: "class",
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		screens: {
+			sm: "640px",
+		},
+		extend: {
+
+
+			textColor: {
+				skin: {
+					base: withOpacity("--color-text-base"),
+					accent: withOpacity("--color-accent"),
+					inverted: withOpacity("--color-fill"),
+				},
+			},
+			backgroundColor: {
+				skin: {
+					fill: withOpacity("--color-fill"),
+					accent: withOpacity("--color-accent"),
+					inverted: withOpacity("--color-text-base"),
+					card: withOpacity("--color-card"),
+					"card-muted": withOpacity("--color-card-muted"),
+				},
+			},
+			outlineColor: {
+				skin: {
+					fill: withOpacity("--color-accent"),
+				},
+			},
+			borderColor: {
+				skin: {
+					line: withOpacity("--color-border"),
+					fill: withOpacity("--color-text-base"),
+					accent: withOpacity("--color-accent"),
+				},
+			},
+			fill: {
+				skin: {
+					base: withOpacity("--color-text-base"),
+					accent: withOpacity("--color-accent"),
+				},
+				transparent: "transparent",
+			},
+			fontFamily: {
+				sans: ["Overpass", "sans-serif"],
+				body: ["Overpass", "sans-serif"],
+				code: ["Fira Mono", "sans-serif"],
+			},
+		}
 	},
 	plugins: [],
 }
