54b87d24bd341cca8de705ecf65ae963b6f9ad38
Ezaz Ahmed <ezaznahmed@gmail.com>
2023-08-22 13:59:16 +0600

    ✅ hero improvement

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,7 +14,7 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		screens: {
-			md: '640px',
+			md: '670px',
 			lg: '1024px',
 			xl: '1500px',
 		},
