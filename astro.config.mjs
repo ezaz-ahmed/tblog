0799fa5478318eabfae977c7ed4dbdcf083f1740
Ezaz Ahmed <ezaznahmed@gmail.com>
2023-08-22 12:08:01 +0600

    🚩 header added

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), tailwind({
     config: {
-      applyBaseStyles: false,
-    },
-  }),]
+      applyBaseStyles: false
+    }
+  }), svelte()]
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,23 +1,39 @@
+import defaultTheme from 'tailwindcss/defaultTheme.js'
+
 function withOpacity(variableName) {
 	return ({ opacityValue }) => {
 		if (opacityValue !== undefined) {
-			return `rgba(var(${variableName}), ${opacityValue})`;
+			return `rgba(var(${variableName}), ${opacityValue})`
 		}
-		return `rgb(var(${variableName}))`;
-	};
+		return `rgb(var(${variableName}))`
+	}
 }
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
 	darkMode: "class",
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		screens: {
-			sm: "640px",
+			md: '640px',
+			lg: '1024px',
+			xl: '1500px',
 		},
 		extend: {
-
-
+			spacing: {
+				'5vw': '5vw', // pull featured sections and navbar in the margin
+				'8vw': '8vw', // positions hero img inside the margin
+				'10vw': '10vw', // page margin
+			},
+			height: {
+				hero: 'min(60rem, calc(100vh - 10rem))', // screen - navbar height (lg: only)
+			},
+			maxWidth: {
+				'8xl': '96rem',
+			},
+			maxHeight: {
+				'50vh': '50vh', // max height for medium size hero images
+				'75vh': '75vh', // max height for giant size hero images
+			},
 			textColor: {
 				skin: {
 					base: withOpacity("--color-text-base"),
@@ -54,9 +70,16 @@ module.exports = {
 				transparent: "transparent",
 			},
 			fontFamily: {
-				sans: ["Overpass", "sans-serif"],
-				body: ["Overpass", "sans-serif"],
-				code: ["Fira Mono", "sans-serif"],
+				sans: ['Matter', ...defaultTheme.fontFamily.sans],
+			},
+			fontSize: {
+				xl: '1.375rem', // 22px
+				'2xl': '1.5625rem', // 25px
+				'3xl': '1.875rem', // 30px
+				'4xl': '2.5rem', // 40px
+				'5xl': '3.125rem', // 50px
+				'6xl': '3.75rem', // 60px
+				'7xl': '4.375rem', // 70px
 			},
 		}
 	},
